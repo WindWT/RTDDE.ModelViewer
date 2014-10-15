f3c8b7fec133db4c6cbffcc471890fec5416c6c3
WindWT <leibao0723@gmail.com>
2014-10-16 01:21:21 +0800

    working with bugs
    wpf 3d transparent z-buffer bug

--- RTDDE.ModelViewer/MainWindow.xaml.cs
+++ RTDDE.ModelViewer/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FreeImageAPI;
+using HelixToolkit.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -23,6 +27,42 @@ namespace RTDDE.ModelViewer
         public MainWindow()
         {
             InitializeComponent();
+            Init3DModel();
+        }
+        private void Init3DModel()
+        {
+            ModelImporter importer = new ModelImporter();
+            Model3DGroup group = importer.Load(@"D:\work\RTD\RTDDE.ModelViewer\RTDDE.ModelViewer\bin\Release\e06_wz_fat_h_01.obj");
+
+            FIBITMAP fiBitmap = FreeImage.Load(FREE_IMAGE_FORMAT.FIF_DDS, @"D:\work\RTD\RTDDE.ModelViewer\RTDDE.ModelViewer\bin\Release\330_e06_wz_fat_h_01.dds", FREE_IMAGE_LOAD_FLAGS.DEFAULT);
+            Bitmap b = FreeImage.GetBitmap(fiBitmap);
+            BitmapSource bitmapSource = BitmapToBitmapSource(b);
+            
+            ImageBrush textureBrush = new ImageBrush(bitmapSource) { ViewportUnits = BrushMappingMode.Absolute, TileMode = TileMode.Tile };
+            var material = new DiffuseMaterial(textureBrush);
+
+            foreach (GeometryModel3D model3d in group.Children)
+            {
+                model3d.Material = material;
+                model3d.BackMaterial = material;
+            }
+
+            viewer.Children.Add(new DefaultLights());
+            model.Content = group;
+        }
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+        private BitmapSource BitmapToBitmapSource(System.Drawing.Bitmap bitmap)
+        {
+            IntPtr ptr = bitmap.GetHbitmap();
+            BitmapSource result =
+                System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    ptr, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            //release resource
+            DeleteObject(ptr);
+
+            return result;
         }
     }
+
 }
